c9284be3dcc5b194ddcd8db4253c212ec292699e
Cheso <otrantodario@gmail.com>
2023-02-26 04:05:32 -0300

    Add Credits Buttons

--- src/CreditsScene/credits.cpp
+++ src/CreditsScene/credits.cpp
@@ -21,6 +21,36 @@ Credits::Credits()
 	this->sceneBackgroundScale = 1.0f;
 	this->sceneBackgroundTint = WHITE;
 
+	width = 40.0f;
+	height = 40.0f;
+	buttonColor = RED;
+	buttonSelectionColor = GREEN;
+	buttonType = SceneType::Credits;
+
+	pos = {width,height * 4.5f};
+	Button* InstagramJ = new Button(pos, width, height, buttonColor, buttonSelectionColor, buttonType);
+	AddButton(InstagramJ);
+
+	pos = { width,height * 6.5f };
+	Button* InstagramM = new Button(pos, width, height, buttonColor, buttonSelectionColor, buttonType);
+	AddButton(InstagramM);
+
+	pos = { width,height * 8.5f };
+	Button* InstagramP = new Button(pos, width, height, buttonColor, buttonSelectionColor, buttonType);
+	AddButton(InstagramP);
+
+	pos = {GetScreenWidth() - width * 2.5f,height * 4.5f };
+	Button* GitE = new Button(pos, width, height, buttonColor, buttonSelectionColor, buttonType);
+	AddButton(GitE);
+
+	pos = { GetScreenWidth() - width * 2.5f,height * 7.5f };
+	Button* ytC = new Button(pos, width, height, buttonColor, buttonSelectionColor, buttonType);
+	AddButton(ytC);
+
+	pos = { GetScreenWidth() - width * 10.0f,height * 11.5f };
+	Button* GitD = new Button(pos, width, height, buttonColor, buttonSelectionColor, buttonType);
+	AddButton(GitD);
+
 	selectionScene = SceneType::Credits;
 	std::cout << "A Credits Scene Was Created" << std::endl;
 }
@@ -278,11 +308,45 @@ void Credits::CheckButtonState()
 		sceneButtons[i]->SetMouseOver(CheckCollisionPointRec(GetMousePosition(), sceneButtons[i]->GetButtonRec()));
 	}
 
-	for (int i = 0; i < sceneButtons.size(); i++)
+	if (sceneButtons[0]->IsButtonPressed())
 	{
-		if (sceneButtons[i]->IsButtonPressed())
-		{
-			selectionScene = sceneButtons[i]->GetButtonType();
-		}
+		selectionScene = sceneButtons[0]->GetButtonType();
+		sceneButtons[0]->SetButtonPresed(false);
+	}
+
+	if (sceneButtons[1]->IsButtonPressed())
+	{
+		//OpenURL("");
+		sceneButtons[1]->SetButtonPresed(false);
+	}
+
+	if (sceneButtons[2]->IsButtonPressed())
+	{
+		//OpenURL("");
+		sceneButtons[2]->SetButtonPresed(false);
+	}
+
+	if (sceneButtons[3]->IsButtonPressed())
+	{
+		//OpenURL("");
+		sceneButtons[3]->SetButtonPresed(false);
+	}
+
+	if (sceneButtons[4]->IsButtonPressed())
+	{
+		OpenURL("https://github.com/BiscupidTea");
+		sceneButtons[4]->SetButtonPresed(false);
+	}
+
+	if (sceneButtons[5]->IsButtonPressed())
+	{
+		OpenURL("https://www.youtube.com/channel/UCOUky79zg0Lc0HweQCkd59Q");
+		sceneButtons[5]->SetButtonPresed(false);
+	}
+
+	if (sceneButtons[6]->IsButtonPressed())
+	{
+		OpenURL("https://github.com/Chesog");
+		sceneButtons[6]->SetButtonPresed(false);
 	}
 }
